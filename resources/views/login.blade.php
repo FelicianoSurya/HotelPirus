f27f2580d596e7b909d0519d3516ebd5537a64a7
FelicianoSurya <suryamarcello@gmail.com>
2021-12-02 11:57:29 +0700

    revisi login and delete icon delete supplier

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,30 +1,37 @@
 @extends('template.appLogin')
 
 @section('custom-css')
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
 @endsection
 
 @section('content-login')
 
-<div class="modal" style="background-color:rgba(0,0,0,0.5);" id="myModal" role="dialog">
+<div class="container-fluid">
+	<div class="row" style="background-color:#85260f">
+		<div class="col-md-12 d-flex justify-content-center align-items-center p-2">
+			<p class="topik">Sistem Informasi Hotel Pirus</p>
+		</div>
+	</div>
+</div>
+<div class="container">
 		<div class="modal-dialog" style="width:100%;margin-top:150px">	
-			<div class="modal-content">
+			<div class="modal-content p-5">
 				<div class="modal-header">
-				<h4 class="modal-title" style="display:flex;justify-content:center; width:100%">Hotel Pirus</h4>
+				<h4 class="modal-title d-flex justify-content-center align-items-center w-100">LOGIN</h4>
 				</div>
 				<div class="modal-body">
 					<form action="login" method="POST">
                         {!! csrf_field() !!}
 						<div class="group-form d-flex align-items-center">
-							<label class="col-3" for="id">ID</label>
+							<label class="col-3" for="id">Id</label>
 							<input type="text" name="username" id="username" placeholder="ID" class="form-control col-8">
 						</div>
 						<div class="group-form d-flex align-items-center" style="margin-top:20px">
 							<label class="col-3" for="">Password</label>
 							<input type="password" name="password" id="password" placeholder="Password" class="form-control col-8">
 						</div>
-                        <div class="d-flex justify-content-center">
-						    <button type="submit" name="submit" class="btn btn-info btn-lg" style="margin-top:20px;">Login</button>
+                        <div class="d-flex justify-content-center mt-3">
+						    <button type="submit" name="submit" class="btn btn-info" style="margin-top:20px;">Login</button>
                         </div>
 						@if($errors->any())
 						<p style="color:red;margin-top:20px;" class="text-center">{{ $errors->first() }}</p>
